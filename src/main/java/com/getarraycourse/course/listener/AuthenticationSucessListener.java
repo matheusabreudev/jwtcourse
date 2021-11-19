e81cab24d5f3799b76eedac93da473bcd8dd4280
Matheus Abreu <matheus.abreu.magalhaes@gmail.com>
2021-11-19 15:55:31 -0300

    Adição de listener para sucesso de autenticação

--- src/main/java/com/getarraycourse/course/listener/AuthenticationSucessListener.java
+++ src/main/java/com/getarraycourse/course/listener/AuthenticationSucessListener.java
@@ -1,5 +1,30 @@
 package com.getarraycourse.course.listener;
 
-public class AuthenticationSucessListener {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.event.EventListener;
+import org.springframework.security.authentication.event.AuthenticationSuccessEvent;
+import org.springframework.stereotype.Component;
 
+import com.getarraycourse.course.domain.UserDom;
+import com.getarraycourse.course.service.LoginAttemptService;
+
+@Component
+public class AuthenticationSucessListener {
+	private LoginAttemptService loginAttemptService;
+	
+	@Autowired
+	public AuthenticationSucessListener(LoginAttemptService loginAttemptService) {
+		this.loginAttemptService = loginAttemptService;
+	}
+	
+	@EventListener
+	public void onAuthenticationSucess(AuthenticationSuccessEvent event) {
+		Object principal = event.getAuthentication().getPrincipal();
+		if(principal instanceof UserDom) {
+			UserDom user = (UserDom) event.getAuthentication().getPrincipal();
+			loginAttemptService.evictUserFromLoginAttemptCache(user.getUsername());
+		}
+	}
+	
+	
 }
